8dbdfaee4309287747f7fbbf2f95f218aa14847a
Abhinay Anand <abhinayanand2005@gmail.com>
2026-04-22 09:24:09 +0530

    Added Simple frontend UI

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Student Profile App Made by Abhinay 🚀"
-
+    return """
+    <h1>Student Profile App</h>
+    <p>Name: Abhinay Anand</p>
+    <p>Class: CS-6C</p>
+"""
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
